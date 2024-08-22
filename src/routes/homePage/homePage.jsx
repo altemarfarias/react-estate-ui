bbd5d643682882bffe3e745f77d68e0071937c42
ALTEMAR FARIAS <acf50mais@gmail.com>
2024-08-21 22:24:52 -0300

    React Real Estate App 50:40

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,8 +1,36 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 function HomePage() {
      return <div className="homePage">
-          <div className="textContainer">Texto</div>
+          <div className="textContainer">
+               <div className="wrapper">
+                    <h1 className="title">
+                         Encontre imóveis e obtenha o Lugar dos seus sonhos
+                    </h1>     
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisci elit, 
+                    sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, 
+                    quis nostrum exercitationem ullam corporis suscipit laboriosam
+                    </p>
+                    <SearchBar />
+                    <div className="boxes">
+                         <div className="box">
+                              <h1>16+</h1>
+                              <h2>Anos de Experiência</h2>
+                         </div>
+                         <div className="box">
+                              <h1>200</h1>
+                              <h2>Prêmios Ganhos</h2>
+                         </div>
+                         <div className="box">
+                              <h1>2000</h1>
+                              <h2>Imóveis Prontos</h2>
+                         </div>
+                    </div>
+               </div>
+          </div>
           <div className="imgContainer">
                <img src="bg.png" alt=""/>
           </div>
